bcdaba26c55feac270f9c1ae9f8c1c49af088a57
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2023-10-08 22:51:40 +0530

    Lecture6-2.cpp

---
+++ Lecture6-2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	//	int i,j,k;
+	//	i=0;
+	//	j=0;
+	//	k=0;
+	//	
+	//	cout<<i<<" "<<j<<" "<<k<<endl;
+	//	j = i++;
+	//	cout<<i<<" "<<j<<" "<<k<<endl;
+	//	k = ++i;
+	//	
+	//	cout<<i<<" "<<j<<" "<<k<<endl;
+	//	cout<<++i<<endl;
+	//	cout<<i;
+
+    int i;
+    for(i=1;i<101;i++) //called the update which happend after the iteratiion(part of a loop is completed)
+	{
+    	cout<<i<<" ";
+	}
+	return 0;
+}
