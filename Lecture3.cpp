c4dc83305428939af2188fc6af5b9af3233ea7be
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2023-10-08 22:39:58 +0530

    Create Lecture3.cpp

---
+++ Lecture3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	//Declare 2 variables and then assign them values of 3,5 and then add them.
+	
+	int a=3;
+	int b=5;
+//	a=3;
+//	b=5;
+	
+//	int c;
+//	c=a+b;
+	
+	cout<<a+b<<endl<<"a+b";
+	
+	cout<<a+b;
+	cout<<endl;
+	cout<<"a+b";
+	return 0;
+}
