d614291bcf119b8fb87d8f54e18cfe3ff28bb4f4
Rjram-10 <118926956+Rjram-10@users.noreply.github.com>
2024-03-31 23:50:23 +0530

    Update Lecture5-2.cpp

--- Lecture5-2.cpp
+++ Lecture5-2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	 int x; // variable name x of type int --> It can store only one value at a time, this is where arrays come into place
-	 
+	  
 	 //Arrays can be used to store multiple values in a single variable
 	 int a[5]; // this means it is an array of name a which can store 5 data items of int type
 	   //Note: The indexing of array starts from 0 to 4 for this case
